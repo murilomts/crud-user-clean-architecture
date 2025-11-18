bceedf8178321f757f98d884ae53e3f3377cdad0
murilomts <murilo.mtds@gmail.com>
2025-11-18 15:29:52 -0300

    first commit

---
+++ src/main/java/com/crud/user/CrudUserCleanArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.crud.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudUserCleanArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudUserCleanArchitectureApplication.class, args);
+	}
+
+}
