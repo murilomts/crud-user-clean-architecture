7304f7bed9170ef6e846f8b732e2ac1dcf2c1c11
murilomtds <murilo.mtds@gmail.com>
2025-11-24 21:21:28 -0300

    Criado domain User

---
+++ src/main/java/com/crud/user/domain/User.java
@@ -0,0 +1,52 @@
+package com.crud.user.domain;
+
+public class User {
+
+    private String cpf;
+    private String name;
+    private String email;
+    private String phone;
+
+    public User(String cpf, String name, String email, String phone) {
+        if (cpf == null || !cpf.matches("\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")) {
+            throw new IllegalArgumentException("Cpf no padrão incorreto!");
+        }
+
+        this.cpf = cpf;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
